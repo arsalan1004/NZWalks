d0fbd796ab75f824138ce0665d1a6fe15ea68368
arsalan1004 <syedarsalan3505@gmail.com>
2024-02-13 19:42:51 +0500

    Feat: Added GET Regions API and DTO layer

--- NZWalks/Controllers/RegionsControllers/RegionsController.cs
+++ NZWalks/Controllers/RegionsControllers/RegionsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.Data;
+using NZWalks.Models.DTO;
 
 namespace NZWalks.Controllers.RegionsControllers;
 [Route("api/[controller]")]
@@ -15,11 +16,46 @@ public class RegionsController : Controller
     }
     
     // GET all Regions
-    // httpp://localhost:5027/api/Regions
+    // http://localhost:5027/api/Regions
     [HttpGet]
     public IActionResult GetAllRegions()
     {
-        var regions = dbContext.Regions.ToList();
-        return Ok(regions);
+        var regionsDomain = dbContext.Regions.ToList();
+
+        var regionsDto = new List<RegionDto>();
+
+        foreach (var region in regionsDomain)
+        {
+            regionsDto.Add(new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageURL = region.RegionImageURL
+            });
+        }
+        return Ok(regionsDto);
+    }
+    
+    // GET REGION BY ID
+    [HttpGet]
+    [Route("{id:guid}")]
+    public IActionResult GetRegById([FromRoute] Guid id)
+    {
+        var region = dbContext.Regions.Find(id);
+
+        if (region == null)
+        {
+            return NotFound();
+        }
+
+        var regionDto = new RegionDto
+        {
+            Id = region.Id,
+            Code = region.Code,
+            Name = region.Name,
+            RegionImageURL = region.RegionImageURL
+        };
+        return Ok(regionDto);
     }
 }
